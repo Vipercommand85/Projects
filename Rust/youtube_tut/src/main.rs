8adf8cc85b0dad977a56b6ee5b938021092c0a1b
Vipercommand85 <wrxdruzzi@gmail.com>
2024-03-29 00:36:52 -0400

    The fun is only just beginning!

--- Rust/youtube_tut/src/main.rs
+++ Rust/youtube_tut/src/main.rs
@@ -5,4 +5,15 @@ fn main() {
     println!("x is: {}", x);
      
 // above is how to embed a variable in an out put line
+    
+    {
+        let x = 2;
+        println!("x is: {}", x);
+    }
+// the above code is in a different scope from the indented curly braces {}
+// this will allow the code to run normally as well as only run inside this scope
+
+    let x = x + 1;
+    println!("x is: {}", x);
+    
 }
